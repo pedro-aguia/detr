b1ee879b177478712425ac426e16e84d38b5c7ea
Pedro Redondo <32511790+CastingSpell@users.noreply.github.com>
2023-09-15 18:31:34 +0200

    revert

--- main.py
+++ main.py
@@ -257,8 +257,9 @@ def main(args):
                     for name in filenames:
                         torch.save(coco_evaluator.coco_eval["bbox"].eval, output_dir / "eval" / name)
 
-    model = torch.load(str(output_dir / "checkpoint.pth"), map_location=torch.device("cpu"))
-    torch.save(model["model"], str(output_dir / "checkpoint.h5"))
+    model = torch.load([output_dir / "checkpoint.pth"], map_location=torch.device("cpu"))
+    torch.save(model["model"], [output_dir / "checkpoint.h5"])
+
     total_time = time.time() - start_time
     total_time_str = str(datetime.timedelta(seconds=int(total_time)))
     print("Training time {}".format(total_time_str))
